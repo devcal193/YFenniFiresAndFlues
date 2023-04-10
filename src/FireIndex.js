8afe202fc148d6a1f30a8946d4b2b05c9a8bdd7e
Calooom193 <callumbeever@hotmail.co.uk>
2023-04-10 16:37:44 +0100

    repopulated URLs with flickr URLs rather than Facebook

--- src/FireIndex.js
+++ src/FireIndex.js
@@ -2,15 +2,15 @@ export const fireCollection = [
    {
       item_id: 666001,
       HeaderImageURL:
-         "https://scontent.fbrs4-1.fna.fbcdn.net/v/t39.30808-6/337558507_940644260304778_4918124472323672833_n.jpg?stp=cp6_dst-jpg&_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=NOkF9IYYfcYAX9JOGiL&_nc_ht=scontent.fbrs4-1.fna&oh=00_AfAGfXrpHEmCvEiiazQWWzaFD4XdOHliUEq0kV3mYcuRFA&oe=6431E60D",
+         "https://live.staticflickr.com/65535/52806408247_8abb1ad1c8_k.jpg",
       imageCollectionURLs: [
          {
-            URL: "https://scontent.fbrs4-1.fna.fbcdn.net/v/t39.30808-6/337558507_940644260304778_4918124472323672833_n.jpg?stp=cp6_dst-jpg&_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=NOkF9IYYfcYAX9JOGiL&_nc_ht=scontent.fbrs4-1.fna&oh=00_AfAGfXrpHEmCvEiiazQWWzaFD4XdOHliUEq0kV3mYcuRFA&oe=6431E60D",
+            URL: "https://live.staticflickr.com/65535/52806408247_8abb1ad1c8_k.jpg",
             description:
                "Lorem ipsum dolor sit amet consectetur adipiscing, elit vitae euismod semper dignissim, curabitur augue lacus congue posuere.",
          },
          {
-            URL: "https://scontent.fbrs4-2.fna.fbcdn.net/v/t39.30808-6/336903451_3360077374230760_9190565430727904002_n.jpg?stp=cp6_dst-jpg&_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=slWe679Gs-8AX8mmcPN&_nc_ht=scontent.fbrs4-2.fna&oh=00_AfCTQ59yGhz43W-3pUQKg-hWLyUjE5jbCGoq4bof98OfBw&oe=6433306C",
+            URL: "https://live.staticflickr.com/65535/52807207139_450d499af7_k.jpg",
 
             description:
                "Lorem ipsum dolor sit amet consectetur adipiscing, elit vitae euismod semper dignissim, curabitur augue lacus congue posuere.",
@@ -24,34 +24,33 @@ export const fireCollection = [
    {
       item_id: 666002,
       HeaderImageURL:
-         "https://scontent.fbrs4-2.fna.fbcdn.net/v/t39.30808-6/253343392_4922119381132140_981041232540410223_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=OHE5SH9P60kAX_PkNAH&_nc_ht=scontent.fbrs4-2.fna&oh=00_AfAZNT8YJ2knQqNjU1qRIT4Iusuqxluyf3FmAFGrLzAYOA&oe=6431F487",
+         "https://live.staticflickr.com/65535/52807413638_ba85732ae1_k.jpg",
       imageCollectionURLs: [
          {
-            URL: "https://scontent.fbrs4-2.fna.fbcdn.net/v/t39.30808-6/253343392_4922119381132140_981041232540410223_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=OHE5SH9P60kAX_PkNAH&_nc_ht=scontent.fbrs4-2.fna&oh=00_AfAZNT8YJ2knQqNjU1qRIT4Iusuqxluyf3FmAFGrLzAYOA&oe=6431F487",
+            URL: "https://live.staticflickr.com/65535/52807413638_ba85732ae1_k.jpg",
             description:
                "Lorem ipsum dolor sit amet consectetur adipiscing, elit vitae euismod semper dignissim, curabitur augue lacus congue posuere.",
          },
          {
-            URL: "https://scontent.fbrs4-1.fna.fbcdn.net/v/t39.30808-6/254580638_4922119934465418_3782124962889742565_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=xwd8YfeShUAAX_LJHmy&_nc_ht=scontent.fbrs4-1.fna&oh=00_AfDCSVeaaI7GkvzZcdtweQnAvguG_NV2fsX64bUTWFWtgw&oe=643316F7",
+            URL: "https://live.staticflickr.com/65535/52806408322_31021a9639_k.jpg",
 
             description:
                "Lorem ipsum dolor sit amet consectetur adipiscing, elit vitae euismod semper dignissim, curabitur augue lacus congue posuere.",
          },
          {
-            URL: "https://www.facebook.com/photo/?fbid=4922089687801776&set=pcb.4922118761132202",
-            // URL: "https://scontent.fbrs4-2.fna.fbcdn.net/v/t39.30808-6/253474230_4922119651132113_5185950942276087967_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=6dqgMqXothQAX840LRq&_nc_ht=scontent.fbrs4-2.fna&oh=00_AfD08QejlHfWPSknyFQoT2DoJegyezLD-FfSbpOyO9DETg&oe=6437F17B",
+            URL: "https://live.staticflickr.com/65535/52806962826_1429406f80_k.jpg",
 
             description:
                "Lorem ipsum dolor sit amet consectetur adipiscing, elit vitae euismod semper dignissim, curabitur augue lacus congue posuere.",
          },
          {
-            URL: "https://scontent.fbrs4-2.fna.fbcdn.net/v/t39.30808-6/254638805_4922121794465232_2697911305038019533_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=r34TvanX7f0AX8V6P_o&_nc_ht=scontent.fbrs4-2.fna&oh=00_AfBHbtKelu4xHNduuYog6rrhQSu1tgbbwqulIk2oTg0zMQ&oe=643721BF",
+            URL: "https://live.staticflickr.com/65535/52806408327_512dbfde37_k.jpg",
 
             description:
                "Lorem ipsum dolor sit amet consectetur adipiscing, elit vitae euismod semper dignissim, curabitur augue lacus congue posuere.",
          },
          {
-            URL: "https://scontent.fbrs4-1.fna.fbcdn.net/v/t39.30808-6/253795353_4922121811131897_7412949573991361644_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=ovNBBVAN51sAX_ZbbMW&_nc_ht=scontent.fbrs4-1.fna&oh=00_AfBDwStkvSxIlgKS7bKsI_Lwb9VNaz9pLX9q_16HRFmAQw&oe=643787FC",
+            URL: "https://live.staticflickr.com/65535/52807371860_c96f89c4b3_k.jpg",
 
             description:
                "Lorem ipsum dolor sit amet consectetur adipiscing, elit vitae euismod semper dignissim, curabitur augue lacus congue posuere.",
@@ -65,69 +64,76 @@ export const fireCollection = [
    {
       item_id: 666003,
       HeaderImageURL:
-         "https://scontent.fbrs4-2.fna.fbcdn.net/v/t1.6435-9/130145496_3907148439295911_6855847100311561927_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=H22shCJ56RkAX_0KiaP&_nc_ht=scontent.fbrs4-2.fna&oh=00_AfAgjh2YN0jzXRWSggiqFeuAyqXGv9TEwwnP4p-8AZ5V7w&oe=6454C827",
+         "https://live.staticflickr.com/65535/52807207274_092bb6277e_k.jpg",
       imageCollectionURLs: [
          {
-            URL: "https://scontent.fbrs4-2.fna.fbcdn.net/v/t1.6435-9/130145496_3907148439295911_6855847100311561927_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=H22shCJ56RkAX_0KiaP&_nc_ht=scontent.fbrs4-2.fna&oh=00_AfAgjh2YN0jzXRWSggiqFeuAyqXGv9TEwwnP4p-8AZ5V7w&oe=6454C827",
+            URL: "https://live.staticflickr.com/65535/52807207274_092bb6277e_k.jpg",
             description:
                "Lorem ipsum dolor sit amet consectetur adipiscing, elit vitae euismod semper dignissim, curabitur augue lacus congue posuere.",
          },
          {
-            URL: "https://scontent.fbrs4-1.fna.fbcdn.net/v/t1.6435-9/129898405_3907148219295933_4024479317896259696_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=NjrnjOmdewoAX9HsE5r&_nc_ht=scontent.fbrs4-1.fna&oh=00_AfAOtZMsDD5WA2HGTqvhaWNuSa6Ivsv1nUe1meYwxpnNFg&oe=6454D24B",
+            URL: "https://live.staticflickr.com/65535/52806408427_e40fad4a73_k.jpg",
 
             description:
                "Lorem ipsum dolor sit amet consectetur adipiscing, elit vitae euismod semper dignissim, curabitur augue lacus congue posuere.",
          },
          {
-            URL: "https://scontent.fbrs4-2.fna.fbcdn.net/v/t1.6435-9/129149190_3907148289295926_5684007337161940537_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=ATl2zDrFlDMAX_5mUeR&_nc_ht=scontent.fbrs4-2.fna&oh=00_AfBK208pvlMJ_1E9HZouW8a_r-NDr2f4B_OcMVzKdDtTig&oe=6454C9FB",
+            URL: "https://live.staticflickr.com/65535/52806408412_c5f2d10441_k.jpg",
 
             description:
                "Lorem ipsum dolor sit amet consectetur adipiscing, elit vitae euismod semper dignissim, curabitur augue lacus congue posuere.",
          },
          {
-            URL: "https://scontent.fbrs4-1.fna.fbcdn.net/v/t1.6435-9/129694426_3907148389295916_1014458912722264549_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=hoAIoQjPq60AX8OXCXX&_nc_oc=AQnNgr8MflVX3YIUpGcZI8eowiWczTnZ-eQbuO0-kYjYBPDbLNVYsTLdorA7UiRm7wo&_nc_ht=scontent.fbrs4-1.fna&oh=00_AfDhCN8gp3-QAUVS38fYuZsKOBX-hg_h2AJBM5w-x7-ztA&oe=6454BD23",
+            URL: "https://live.staticflickr.com/65535/52807371905_10a1d9e8c6_b.jpg",
+
+            description:
+               "Lorem ipsum dolor sit amet consectetur adipiscing, elit vitae euismod semper dignissim, curabitur augue lacus congue posuere.",
+         },
+         {
+            URL: "https://live.staticflickr.com/65535/52806408467_0b3273cdd3_b.jpg",
 
             description:
                "Lorem ipsum dolor sit amet consectetur adipiscing, elit vitae euismod semper dignissim, curabitur augue lacus congue posuere.",
          },
       ],
-      make_model: "Heta Scan-Line 900",
-      description: "Blackwood Soapstone",
+      make_model: "Wiking Miro 1",
+      description:
+         "Lorem ipsum dolor sit amet consectetur adipiscing, elit vitae euismod semper dignissim, curabitur augue lacus congue posuere.",
       featured: false,
    },
 
    {
       item_id: 666004,
       HeaderImageURL:
-         "https://scontent.fbrs4-2.fna.fbcdn.net/v/t1.6435-9/119700093_3680391978638226_1771556034733059994_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=D-IKAVSMpDgAX8Qcq_1&_nc_ht=scontent.fbrs4-2.fna&oh=00_AfD9P2MXFBKQLJA6ZonXGajt8-ZyPLifJEzfITF_shoYng&oe=6454D2D7",
+         "https://live.staticflickr.com/65535/52807372155_f5b83d1fd7_b.jpg",
 
       imageCollectionURLs: [
          {
-            URL: "https://scontent.fbrs4-2.fna.fbcdn.net/v/t1.6435-9/119700093_3680391978638226_1771556034733059994_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=D-IKAVSMpDgAX8Qcq_1&_nc_ht=scontent.fbrs4-2.fna&oh=00_AfD9P2MXFBKQLJA6ZonXGajt8-ZyPLifJEzfITF_shoYng&oe=6454D2D7",
+            URL: "https://live.staticflickr.com/65535/52807372155_f5b83d1fd7_b.jpg",
 
             description:
                "Lorem ipsum dolor sit amet consectetur adipiscing, elit vitae euismod semper dignissim, curabitur augue lacus congue posuere.",
          },
          {
-            URL: "https://scontent.fbrs4-1.fna.fbcdn.net/v/t1.6435-9/119951876_3680391448638279_1802870148693396389_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=m4obGqWj23kAX9m-vcK&_nc_ht=scontent.fbrs4-1.fna&oh=00_AfByRxQDO0SOLvupKVCmSaV8JmD6GVBOBiR5_UuDPpcbBA&oe=6454C745",
+            URL: "https://live.staticflickr.com/65535/52807207344_f38d5e6b20_b.jpg",
 
             description:
                "Lorem ipsum dolor sit amet consectetur adipiscing, elit vitae euismod semper dignissim, curabitur augue lacus congue posuere.",
          },
          {
-            URL: "https://scontent.fbrs4-1.fna.fbcdn.net/v/t1.6435-9/119966873_3680391678638256_8121038456600359636_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=KZBnEeaz5X8AX_7JzP3&_nc_ht=scontent.fbrs4-1.fna&oh=00_AfCuRXlW8dZfrRoZGvt6Z1iZ4SPxBVBrXYBcGvV8-syNWg&oe=64549F56",
+            URL: "https://live.staticflickr.com/65535/52807207299_2aaa3d04ea_b.jpg",
 
             description:
                "Lorem ipsum dolor sit amet consectetur adipiscing, elit vitae euismod semper dignissim, curabitur augue lacus congue posuere.",
          },
          {
-            URL: "https://scontent.fbrs4-1.fna.fbcdn.net/v/t1.6435-9/119810707_3680391748638249_2371535689092378025_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=iQO7vD4UeTAAX8MGWGE&_nc_ht=scontent.fbrs4-1.fna&oh=00_AfAmXpoU1xvXcJ7rmcKHDKUGT_UK5s2VpeZav7N5iD-ITw&oe=6454D1DF",
+            URL: "https://live.staticflickr.com/65535/52807413858_a1de7e2786_b.jpg",
 
             description:
                "Lorem ipsum dolor sit amet consectetur adipiscing, elit vitae euismod semper dignissim, curabitur augue lacus congue posuere.",
          },
          {
-            URL: "https://scontent.fbrs4-1.fna.fbcdn.net/v/t1.6435-9/119953752_3680391835304907_1673231247843757138_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=dLXPAbU86rQAX9YvQ6B&_nc_ht=scontent.fbrs4-1.fna&oh=00_AfDbMj05IZf8XKCiSR1z--YMkGnio0WCzEwklmuK5mApVw&oe=6454CC23",
+            URL: "https://live.staticflickr.com/65535/52806962941_4ddc2814f6_b.jpg",
 
             description:
                "Lorem ipsum dolor sit amet consectetur adipiscing, elit vitae euismod semper dignissim, curabitur augue lacus congue posuere.",
